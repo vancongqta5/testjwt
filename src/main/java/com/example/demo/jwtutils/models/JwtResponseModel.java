79c3095bb893c9e3f742b863d5eb21808744f216
vancongqta5 <vancongqta5@gmail.com>
2023-03-20 23:02:29 +0700

    test Spring Security - JWT editdone-run-NO

---
+++ src/main/java/com/example/demo/jwtutils/models/JwtResponseModel.java
@@ -0,0 +1,12 @@
+package com.example.demo.jwtutils.models;
+import java.io.Serializable;
+public class JwtResponseModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private final String token;
+    public JwtResponseModel(String token) {
+        this.token = token;
+    }
+    public String getToken() {
+        return token;
+    }
+}
